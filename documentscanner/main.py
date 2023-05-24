09745db6301ec45a74436253d3e8bef10f9ad803
Juan Urdaneta <jurdanetac10@gmail.com>
2023-05-24 10:40:45 -0400

    Add bot token loading from file

--- documentscanner/main.py
+++ documentscanner/main.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TOKEN_PATH = f"{ROOT_DIR}/token"
+
+try:
+    with open(TOKEN_PATH, 'r') as file:
+        TOKEN = file.read().strip()
+except FileNotFoundError:
+    print("Bot token file not found")
+    sys.exit(1)
+
 print("Hello, world!")
+print(TOKEN)
