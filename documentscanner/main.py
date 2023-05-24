754fd6dbc0cfac4d4df0fcb4f75a106aa9ea1567
Juan Urdaneta <jurdanetac10@gmail.com>
2023-05-24 15:25:54 -0400

    Respond to `/start` command

--- documentscanner/main.py
+++ documentscanner/main.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+import logging
 import os
 import sys
+from telegram import Update
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+)
+
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TOKEN_PATH = f"{ROOT_DIR}/token"
 
+
+# Load bot token
 try:
     with open(TOKEN_PATH, "r") as file:
         TOKEN = file.read().strip()
@@ -14,5 +25,25 @@ except FileNotFoundError:
     print("Bot token file not found")
     sys.exit(1)
 
-print("Hello, world!")
-print(TOKEN)
+
+# Set logging to console
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
+
+
+if __name__ == "__main__":
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    start_handler = CommandHandler("start", start)
+    application.add_handler(start_handler)
+
+    # Run app
+    application.run_polling()
